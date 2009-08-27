01ba4ad448fd8f7f861f075c8e626ae6319e7529
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2009-08-27 14:18:03 +0000

    cleanup gpx_parser.py

--- gpx2googlemaps/gpx_parser.py
+++ gpx2googlemaps/gpx_parser.py
@@ -28,15 +28,6 @@ from xml.dom import minidom
 from cgi import escape, FieldStorage
 
 
-#class gpx2googlemaps(PyLucidBasePlugin):
-#
-#    def lucidTag(self):
-#        self.display("0")
-#
-#    def display(self, function_info=None):
-#        context = {}
-#        self._render_template("display", context)#, debug=True)
-#   
 
 class Waypoint:
     def __init__(self, node):
@@ -88,91 +79,6 @@ class GPX_Parser:
         #print times
         points = [self.tracks[name][time] for time in times.sort()]
         return [(point['lat'], point['lon']) for point in points]
-
-    def print_map(self):
-
-        try:
-            print u"""
-            <script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=XXX" type="text/javascript"></script>
-          <script language="javascript">
-          function plus(element)
-          {
-             element.parentNode.children("plus").className="hidden";
-             element.parentNode.children("small").className="hidden";
-             element.parentNode.children("minus").className="visible";
-             element.parentNode.children("large").className="visible";
-          }
-          function minus(element)
-          {
-             element.parentNode.children("plus").className="visible";
-             element.parentNode.children("small").className="visible";
-             element.parentNode.children("minus").className="hidden";
-             element.parentNode.children("large").className="hidden";
-          }
-          function wheelevent(e)
-          {
-             if (!e) e = window.event;
-             if (e.preventDefault) e.preventDefault();
-             e.returnValue = false;
-          }
-          </script>
-        
-            """
-            print u"""<html>
-               <body onload="load()" onunload="GUnload()">
-                <div id="map" style="width: 100%; height: 500px; position: relative; background-color: rgb(229, 227, 223);"></div>
-          <script type='text/javascript'>
-         
-        
-          function load() {
-            if (GBrowserIsCompatible()) {
-              function createMarker(point,markerOptions,Name) {
-                var marker = new GMarker(point, markerOptions);
-                GEvent.addListener(marker, "click", function() {
-                  marker.openInfoWindowHtml(Name);
-                });
-                GEvent.addListener(marker,"mouseover", function() {
-                  marker.openInfoWindowHtml(Name);
-                });
-                return marker;
-              }
-        
-              var map = new GMap2(document.getElementById("map"));
-              //var mgr = new MarkerManager(map);
-	      map.setCenter(new GLatLng(50.00, 12.00), 13)
-              map.addControl(new GLargeMapControl()); <!-- Adds zoom control -->
-              map.addMapType(G_PHYSICAL_MAP);
-              map.addControl(new GHierarchicalMapTypeControl());  <!-- Adds hybrid/street/satellite -->
-              var gcIcon = new GIcon(G_DEFAULT_ICON);      
-              gcIcon.image = "http://www.wathoserver.de/~gc/geocache-found.png";
-              gcIcon.iconSize = new GSize(24,24);
-              gcIcon.shadow="";
-              gcIcon.iconAnchor = new GPoint(12,12);      
-              gcIcon.infoWindowAnchor = new GPoint(24,0);
-              markerOptions = { icon:gcIcon };
-              var bounds = new GLatLngBounds();"""
-            for wpt in self.waypoints:
-                print u"""      
-              var latlng = new GLatLng(%s, %s);      
-              map.addOverlay(createMarker(latlng, markerOptions, '%s<hr/>%s<div><a href="http://www.geocaching.com/seek/cache_details.aspx?wp=%s" target="_blank">geocaching.com</a></div>'));
-              bounds.extend(latlng)""" % (
-                  wpt.lat, wpt.lon, escape(wpt.name, True), escape(wpt.desc, True), escape(wpt.name))
-            print u"""        
-              //map.setCenter(bounds.getCenter(), 11, G_HYBRID_MAP);
-	      map.setCenter(bounds.getCenter())
-              map.setZoom(map.getBoundsZoomLevel(bounds));
-              map.enableScrollWheelZoom();     
-            }
-}
-         //     GEvent.addDomListener(map, "DOMMouseScroll", wheelevent);
-         // map.onmousewheel = wheelevent;
-          
-          
-          </script>          
-        <span class='last_update'>Letzte Aktualisierung: %s</span>""" % self.last_update
-        except Exception, e:
-            print e
-
 
 
 if __name__ == '__main__':
